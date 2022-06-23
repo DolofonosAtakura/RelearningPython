c0e553062129ff20e46ab877671a3ed51838b82d
Dolofonos <100103968+DolofonosAtakura@users.noreply.github.com>
2022-06-23 14:41:08 -0500

    created a new, faster function
    this function brings everything into memory instead of letting it read each line individually

--- userCSVtoDict/userCSVtoDict.py
+++ userCSVtoDict/userCSVtoDict.py
@@ -1,5 +1,15 @@
-#translates a user CSV to a dictionary, works with or without blank line at bottom in testing
+# to-do: 
+# not do a users sub array so you can look up usernames afterwards
+
+# import time
+# ^removed because i was curious on code speed:
+# usersCSVtoDict: 0.0013560000000000013
+# usersChanged:   0.00012119999999999839
+# roughly 10x faster
+
+# translates a user CSV to a dictionary, currently requires a blank line at the bottom to function, can add try/except to fix
 def usersCSVtoDict(infile):
+    # start = time.perf_counter()
     d = dict()
 
     with open(infile) as a:
@@ -17,13 +27,41 @@ def usersCSVtoDict(infile):
                     d[f"User{count}"].update({f"{keys[i]}":r[count2]})
                     count2 += 1                    
             count += 1
-
+    # print(time.perf_counter()-start)
     return d
+
+def usersChanged(infile):
+    # start = time.perf_counter()
+    d = dict()
+
+    with open(infile) as a:
+        fileInfo = a.read()
+
+    slist = fileInfo.split()
+    del a, fileInfo
+    count = 0
+    keys = slist[0].lower().strip().split(",")
+
+    for i in range(1,len(slist)):
+        r = slist[i].split(",")
+        ind = i - 1
+        d.update({f"User{ind}":{}})
+
+        for j in range(0,len(keys)):
+            d[f"User{ind}"].update({f"{keys[j]}":r[j]})
+        
+    # print(time.perf_counter()-start)
+    return(d)
+
 
 def main():
     file = "userinfo.csv"
-    q = usersCSVtoDict(file)
-    print(q)
+    q1 = usersCSVtoDict(file)
+    q2 = usersChanged(file)
+    if q1 == q2:
+        print(q1)
+    else:
+        print("not the same")
 
 if __name__ == '__main__':
     main()
